8c0c6e900b5a3ba5bedae668c232eda3c210292d
Labbi Ahmed <labbi5689@gmail.com>
2024-06-03 19:50:38 +0600

    project start with basic folder setup and db

---
+++ src/test/java/com/labbi/TaskTracker/TaskTrackerApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labbi.TaskTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
